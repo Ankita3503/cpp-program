4c18775a157f908582e8f531558bc51fe17087d4
Ankita3503 <94631478+Ankita3503@users.noreply.github.com>
2021-11-19 21:14:59 +0530

    Add files via upload

---
+++ classn object.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+ class student
+ {
+ private:
+ int r,marks;
+ public:
+  
+   
+void display()
+   
+ 	{
+ 	r=3;
+ 	marks=90;
+	 }
+};
+ 	
+/*void enter()
+     cout<<"r="<<r;
+ 	cout<<"marks="<<marks;	 	
+ };*/
+ int main()
+ {
+ student obj;
+ obj.display();
+ 
+ getch();	
+ }
