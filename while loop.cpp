d0edfd2f7bdece04e036918a6b8f033ae3638360
Ankita3503 <94631478+Ankita3503@users.noreply.github.com>
2021-11-21 22:27:18 +0530

    Add files via upload

---
+++ while loop.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+int i=1,sum=0;
+do
+
+{
+cout<<i<<endl;
+i++;	
+}
+while(i<=5);
+sum=sum+i;
+cout<<"sum="<<sum;
+getch();	
+}
