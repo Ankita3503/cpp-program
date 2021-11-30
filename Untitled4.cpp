17d827b82c843ef3d845a79dfc314b7d9944b7da
Ankita3503 <94631478+Ankita3503@users.noreply.github.com>
2021-11-30 23:19:05 +0530

    Add files via upload

---
+++ Untitled4.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+int i=1,x;
+cout<<"enter any number";
+cin>>x;
+
+while(i<=10)
+{
+	
+cout<<x*i<<endl;
+i++;	
+
+}
+
+
+
+getch();	
+}
