b39896e3d695b39f2c503325394e1a1b7e799bed
Ankita3503 <94631478+Ankita3503@users.noreply.github.com>
2021-12-01 23:40:06 +0530

    Update 2Darray.cpp

--- 2Darray.cpp
+++ 2Darray.cpp
@@ -2,8 +2,8 @@
 using namespace std;
 int main()
 {
-    int i,j ,a[3][3];
-    cout<<"enter the elements of 2d array";
+    int i,j ,a[3][3],b[3][3],c[3][3];
+    cout<<"enter the elements of  3*3 matrix \n";
     for(i=0;i<3;i++)
     {
         for(j=0;j<3;j++)
@@ -11,7 +11,7 @@ int main()
             cin>>a[i][j];
         }
     }
-    cout<<"matix representation of 2d array\n";
+    cout<<"matix A=  \n";
     for(i=0;i<3;i++)
     {
         for(j=0;j<3;j++)
@@ -19,6 +19,33 @@ int main()
         cout<<a[i][j]<<"\t";
         }
         cout<<"\n";
+    }
+     cout<<"enter the elements of 3*3 matrix";
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            cin>>a[i][j];
+        }
+    }
+    cout<<"matix B= \n";
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+        cout<<b[i][j]<<"\t";
+        }
+        cout<<"\n";
+    }
+    cout<<"matrix addition \n";
+ for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+           c[i][j]=a[i][j]+b[i][j];
+            cout<<c[i][j]<<"\t";
+        }
+     cout<<"\n";
     }
 return 0;
 }
