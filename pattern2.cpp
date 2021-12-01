cee2343286006695f31161f2132dd5d9b28c6476
Ankita3503 <94631478+Ankita3503@users.noreply.github.com>
2021-12-01 23:47:15 +0530

    Add files via upload

--- pattern2.cpp
+++ pattern2.cpp
@@ -1,16 +1,18 @@
 #include<iostream>
-#include<conio.h>
 using namespace std;
 int main()
 {
-int i,j;
-for(i=5;i>=1;i--)
+    int i,j;
+for(i=0;i<6;i++)
 {
-for(j=1;j<=5;j++)
-{
-cout<<j;	
+    for(j=0;j<6;j++)
+    {
+        if(j<6-i)
+        {
+        cout<<"*";
+        }
+    }
+    cout<<"\n";
 }
-cout<<endl;	
-}
-getch();	
+    return 0;
 }
