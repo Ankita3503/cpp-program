145dafd157decf4b22eefd4f0b86946ab13a25b5
Ankita3503 <94631478+Ankita3503@users.noreply.github.com>
2021-12-22 23:20:13 +0530

    Update delete_array1.cpp

--- delete_array1.cpp
+++ delete_array1.cpp
@@ -5,17 +5,17 @@ using namespace std;
 int main()
 {
     int  size,item,i;
-    cout<<"enter the size of array";
+    cout<<"Enter the size of array";
     cin>>size;
     int a[size];
-    cout<<"enter the element of array";
+    cout<<"Enter the element of array";
     for( i=0;i<size;i++)
     {
         cin>>a[i];
     }
   
    
-    cout<<"new array :-\n";
+    cout<<"New array :-\n";
     for(i=0;i<size-1;i++)
     {
         cout<<a[i]<<"\n";
