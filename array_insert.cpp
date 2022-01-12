a6a265ebd17129b95efd77955fb420445f04988e
Ankita3503 <94631478+Ankita3503@users.noreply.github.com>
2022-01-12 21:21:34 +0530

    Update array_insert.cpp

--- array_insert.cpp
+++ array_insert.cpp
@@ -1,25 +1,25 @@
-/* insert an element in a array   at the end of array */
+/* insert an element in a array at the end of array */
 
 #include<iostream>
 using namespace std;
 int main()
 {
     int  size,item,i;
-    cout<<"enter the size of array";
+    cout<<"Enter the size of array : ";
     cin>>size;
     int a[size];
-    cout<<"enter the element of array";
+    cout<<"Enter the elements of array\n";
     for( i=0;i<size-1;i++)
     {
         cin>>a[i];
     }
-    cout<<"enter the element to be inserted= ";
+    cout<<"Enter the element to be inserted= ";
     cin>>item;
     a[size]=item;
-    cout<<"new array :-\n";
+    cout<<"New array : \n";
     for(i=0;i<size;i++)
     {
-        cout<<a[i]<<"\n";
+        cout<<a[i]<<" ";
     }
     return 0;
 }
