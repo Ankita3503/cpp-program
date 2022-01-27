cfdb262c1d4a6d123d7147c1c62d0efde1a83024
ankita <94631478+Ankita3503@users.noreply.github.com>
2022-01-27 22:13:34 +0530

    Add files via upload

---
+++ arrange_ascending_order.cpp
@@ -0,0 +1,27 @@
+
+// sort an array without using any sorting method 
+// sorting by sort function 
+
+#include<iostream>
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int T;
+    cin>>T;
+    int a[T];
+    for(int i=0;i<T;i++)
+    {
+        cin>>a[i];
+    }
+    
+    
+       sort(a,a+T);
+   
+    for(int i=0;i<T;i++)
+    {
+        cout<<a[i]<<endl;
+    }
+   
+    return 0;
+}
