85046bf7c6054c861725aeb053d9afca4ef2a8c8
ankita <94631478+Ankita3503@users.noreply.github.com>
2022-01-30 22:05:27 +0530

    Add files via upload

---
+++ technicalpattern1.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int i,j;
+  for(i=65;i<=69;i++)
+  {
+
+      for(j=65;j<=69;j++)
+      {
+          if(j<=i)
+          {
+              cout<<char(j);
+          }
+         
+         
+      }
+          cout<<"\n";
+  }
+    return 0;
+}
+
