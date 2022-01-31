338a0e9ea33e5b69893e0717cc77f0fce0b0304d
ankita <94631478+Ankita3503@users.noreply.github.com>
2022-01-31 22:09:23 +0530

    Add files via upload

---
+++ hosted_codechef.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int T;
+	cin>>T;
+	while(T--)
+	{
+	    int n;
+	    cin>>n;
+	    if(n==2010||n==2015||n==2016||n==2017||n==2019)
+	    {
+	        cout<<"HOSTED\n";
+	    }
+	    else
+	    {
+	        cout<<"NOT HOSTED\n";
+	    }
+	}
+	return 0;
+}
