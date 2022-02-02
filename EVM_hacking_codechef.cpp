5e5362e1d8780ecb89d6ab284584257b234b1131
ankita <94631478+Ankita3503@users.noreply.github.com>
2022-02-02 22:27:32 +0530

    Add files via upload

---
+++ EVM_hacking_codechef.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int a,b,c,p,q,r;
+	    cin>>a>>b>>c>>p>>q>>r;
+	   int x=(p+q+r)/2;
+	  
+	    if((p+b+c)>x||(a+q+c)>x||(a+b+r)>x)
+	    {
+	        cout<<"Yes\n";
+	    }
+	    else
+	    {
+	        cout<<"No\n";
+	    }
+	}
+	return 0;
+}
