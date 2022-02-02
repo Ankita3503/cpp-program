35f6ffd249970926b1dd3834f448f6aff4269460
ankita <94631478+Ankita3503@users.noreply.github.com>
2022-02-02 22:26:14 +0530

    Add files via upload

---
+++ badminton_serves.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+    while(t--)
+     {
+	    long long  num;
+	    cin>>num;
+	    cout<<(num/2)+1<<"\n";
+	}
+	return 0;
+}
