2c54695cc4975f94d46e9981a4765f6f6f5ed63e
ankita <94631478+Ankita3503@users.noreply.github.com>
2022-02-04 23:37:53 +0530

    Create chef_and_hair_salon_codechef.cpp

---
+++ chef_and_hair_salon_codechef.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+	// your code goes here
+	int T;
+	cin>>T;
+	while(T--)
+	{
+	    int no_of_customer,times;
+	    cin>>no_of_customer>>times;
+	    cout<< no_of_customer*times<<"\n";
+	}
+	return 0;
+}
