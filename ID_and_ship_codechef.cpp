d83cb9921b9981c0e012bd42cc72c6504afc3f42
ankita <94631478+Ankita3503@users.noreply.github.com>
2022-02-06 22:54:09 +0530

    Update ID_and_ship_codechef.cpp

--- ID_and_ship_codechef.cpp
+++ ID_and_ship_codechef.cpp
@@ -1,35 +1,29 @@
 #include <iostream>
 using namespace std;
-
-int main() {
+int main()
+{
 int T;
 cin>>T;
 while(T--)
 {
   char Ch;
   cin>>Ch;
- 
   if(Ch=='B'||Ch=='b')
   {
       cout<<"BattleShip\n";
-      
   }
    else if(Ch=='C'||Ch=='c')
   {
       cout<<"Cruiser\n";
-      
   }
    else if(Ch=='D'||Ch=='d')
   {
       cout<<"Destroyer\n";
-     
   }
    else if(Ch=='f'||Ch=='F')
   {
       cout<<"Frigate\n";
-     
-      
   }
 }
-	return 0;
+return 0;
 }
