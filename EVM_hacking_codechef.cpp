5c3e15e3db4e9129ee13c3d36f74a33b8222232a
ankita <94631478+Ankita3503@users.noreply.github.com>
2022-02-06 22:52:59 +0530

    Update EVM_hacking_codechef.cpp

--- EVM_hacking_codechef.cpp
+++ EVM_hacking_codechef.cpp
@@ -1,16 +1,14 @@
 #include <iostream>
 using namespace std;
-
-int main() {
-	// your code goes here
+int main()
+{
 	int t;
 	cin>>t;
 	while(t--)
 	{
 	    int a,b,c,p,q,r;
 	    cin>>a>>b>>c>>p>>q>>r;
-	   int x=(p+q+r)/2;
-	  
+	    int x=(p+q+r)/2;
 	    if((p+b+c)>x||(a+q+c)>x||(a+b+r)>x)
 	    {
 	        cout<<"Yes\n";
