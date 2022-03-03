24700adaa31db695440bdcd36ce9348750ffafd8
ankita <94631478+Ankita3503@users.noreply.github.com>
2022-03-04 00:07:23 +0530

    Add files via upload

---
+++ string.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<conio.h>
+#include<string.h>
+using namespace std;
+int main()
+{
+    char name1[]="ankita";
+    char name2[]="kumari";
+        cout<< strcat(name1 ,name2);    // concate two string 
+    return 0;
+}
