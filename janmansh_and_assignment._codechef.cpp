9c77477d90218bcdb9bfcd4e2c5dcc8519ee1206
ankita <94631478+Ankita3503@users.noreply.github.com>
2022-03-05 22:35:14 +0530

    Create janmansh_and_assignment._codechef.cpp

---
+++ janmansh_and_assignment._codechef.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n;
+	    cin>>n;
+	    if((10-n)>=3)
+	    {
+	        cout<<"yes\n";
+	    }
+	    else
+	    {
+	        cout<<"no\n";
+	    }
+	}
+	return 0;
+}
