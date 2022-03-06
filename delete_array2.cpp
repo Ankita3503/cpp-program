894541d083ec588e3ae5c8ad32dc2be4242e6c59
ankita <94631478+Ankita3503@users.noreply.github.com>
2022-03-06 23:08:14 +0530

    Update delete_array2.cpp

--- delete_array2.cpp
+++ delete_array2.cpp
@@ -5,21 +5,25 @@ using namespace std;
 int main()
 {
     int  size,item,i;
-    cout<<"enter the size of array";
+    cout<<"Enter the size of array";
     cin>>size;
     int a[size];
-    cout<<"enter the element of array";
+    cout<<"Enter the element of array";
     for( i=0;i<size;i++)
     {
         cin>>a[i];
     }
-   
+    cout<<"Array are:- ";
+    for(int i=0;i<size;i++)
+    {
+        cout<<a[i]<<" ";
+    }
    for(i=1;i<size;i++)
        {
      a[i-1]=a[i];
        }
        
-    cout<<"new array :-\n";
+    cout<<"New Array :-\n";
     for(i=0;i<size-1;i++)
     {
         cout<<a[i]<<"\n";
