7950ccc7215fa60fe5812bd14fba2431b2b664b7
ankita <94631478+Ankita3503@users.noreply.github.com>
2022-03-06 23:04:05 +0530

    Update choice.cpp

--- choice.cpp
+++ choice.cpp
@@ -10,34 +10,34 @@ cin>>a;
 if(a==1)
      {
      cout<<"\n area of rectangle";
-	 cout<<"\n enter the length  of  rectangle=";
+	 cout<<"\n Enter the length  of  rectangle=";
 	 cin>>l;
-	 cout<<"\n enter the breadth  of  rectangle=";
+	 cout<<"\n Enter the breadth  of  rectangle=";
 	 cin>>b;
 	 area=l*b;
-	 cout<<"\n area of  rectangle"<<area;	
+	 cout<<"\n Area of  rectangle"<<area;	
 	 }
 	 if (a==2)
 	 {
-	 cout<<	"\n area of triangle";
-	 cout<<"\n enter the height of  triangle";
+	 cout<<	"\n Area of triangle";
+	 cout<<"\n Enter the height of  triangle";
 	 cin>>h;
-	 cout<<"\n enter the  base of  triangle";
+	 cout<<"\n Enter the  base of  triangle";
 	 cin>>base;
 	 area=h*base;
-	 cout<<"\n area of  triangle"<<area;	
+	 cout<<"\n Area of  triangle"<<area;	
 	 }
 	  else if(a==3)
 	 {
-	 cout<<"\n area of circle";
-	 cout<<"\n enter the radius of circle=";
+	 cout<<"\n Area of circle";
+	 cout<<"\n Enter the radius of circle=";
 	 cin>>r;
 	 area=$*r*r;
 	 cout<<"\n area of  circle="<<area;	
 	 }
 	 else if(a<4)
 	 {
-	 cout<<"please try again";	
+	 cout<<"Please try again";	
 	 }	
 getch();
 }
