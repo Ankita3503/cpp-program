a82f84a685110fea073499771bf94d081dc26239
ankita <94631478+Ankita3503@users.noreply.github.com>
2022-03-11 23:52:55 +0530

    Create bath_in_winters_codechef.cpp

---
+++ bath_in_winters_codechef.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int x,y;
+	    cin>>x>>y;
+	  
+	   x=x/ (2*y);
+	       cout<<x<<"\n";
+	  
+	}
+	return 0;
+}
