740695ab99ffcff5c85ba19e335a1141c9725fd5
ankita <94631478+Ankita3503@users.noreply.github.com>
2022-03-12 23:24:14 +0530

    Update badminton_serves.cpp

--- badminton_serves.cpp
+++ badminton_serves.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
 using namespace std;
-int main() {
-	// your code goes here
+int main() 
+{
 	int t;
 	cin>>t;
-    while(t--)
-     {
+        while(t--)
+          {
 	    long long  num;
 	    cin>>num;
 	    cout<<(num/2)+1<<"\n";
-	}
-	return 0;
+	   }
+return 0;
 }
