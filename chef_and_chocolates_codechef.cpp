0c12658caff59cc84b1613046b627e59efb5b507
ankita <94631478+Ankita3503@users.noreply.github.com>
2022-03-16 23:56:30 +0530

    Create chef_and_chocolates_codechef.cpp

---
+++ chef_and_chocolates_codechef.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int c,x,y;
+	    cin>>c>>x>>y;
+	    cout<<(c-x)*y<<"\n";
+	}
+	
+	return 0;
+}
