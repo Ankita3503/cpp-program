b7d315ca89f4dfc5ab781f7406763bc0b9e7e8f8
ankita <94631478+Ankita3503@users.noreply.github.com>
2022-03-16 23:54:52 +0530

    Create balanced_reversals_codechef.cpp

---
+++ balanced_reversals_codechef.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n;
+	    cin>>n;
+        string s1;
+        cin>>s1;
+        sort(s1.begin(),s1.end());
+        cout<<s1<<"\n";
+	    
+	}
+	return 0;
+}
