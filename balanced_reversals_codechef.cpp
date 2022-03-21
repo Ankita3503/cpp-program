d43c2d9e0d247674e4f69cca646a8305135cde41
ankita <94631478+Ankita3503@users.noreply.github.com>
2022-03-21 22:23:05 +0530

    Update balanced_reversals_codechef.cpp

--- balanced_reversals_codechef.cpp
+++ balanced_reversals_codechef.cpp
@@ -1,20 +1,18 @@
 #include <iostream>
 #include<bits/stdc++.h>
 using namespace std;
-
-int main() {
-	// your code goes here
+int main()
+{
 	int t;
 	cin>>t;
 	while(t--)
 	{
-	    int n;
-	    cin>>n;
+	int n;
+	cin>>n;
         string s1;
         cin>>s1;
-        sort(s1.begin(),s1.end());
-        cout<<s1<<"\n";
-	    
+        sort(s1.begin(),s1.end());  //sorting the string
+        cout<<s1<<"\n";    
 	}
 	return 0;
 }
