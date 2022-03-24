a8ef650e8de106659efb91d081c0be9710d6e0a8
ankita <94631478+Ankita3503@users.noreply.github.com>
2022-03-24 23:09:47 +0530

    Create lazy_chef_Codechef.cpp

---
+++ lazy_chef_Codechef.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int x,m,d;
+	    cin>>x>>m>>d;
+	    if(x*m < x+d)
+	    {
+	        cout<<x*m<<"\n";
+	    }
+	    else
+	    cout<<x+d<<"\n";
+	}
+	return 0;
+}
