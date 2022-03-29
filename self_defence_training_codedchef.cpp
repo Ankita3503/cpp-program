04bf52244ed27250dcc91b99d40f754fc7bbb5d0
ankita <94631478+Ankita3503@users.noreply.github.com>
2022-03-29 23:36:57 +0530

    Create self_defence_training_codedchef.cpp

---
+++ self_defence_training_codedchef.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n;
+	    cin>>n;
+	    int a[n];
+	    for(int i=0;i<n;i++)
+	    {
+	        cin>>a[i];
+	    }
+	    int count=0;
+	   for(int i=0;i<n;i++)
+	   {
+	       if(a[i]>=10 && a[i]<=60 )
+	       count++;
+	   }
+	   cout<<count<<"\n";
+	    
+	}
+	return 0;
+}
