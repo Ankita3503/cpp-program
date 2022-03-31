1dec98e7e15fee2e47d674fb01832891de89b004
ankita <94631478+Ankita3503@users.noreply.github.com>
2022-03-31 23:10:24 +0530

    Create chefs and profits codechef.cpp

---
+++ chefs and profits codechef.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int x,y,z;
+	cin>>x>>y>>z;
+	int a=x*y;
+	int b=x*z;
+	cout<<b-a<<"\n";
+	}
+	
+	return 0;
+}
