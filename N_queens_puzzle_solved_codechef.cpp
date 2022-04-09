a28059d7322912ca3dfb3c2442dc868478b94baf
ankita <94631478+Ankita3503@users.noreply.github.com>
2022-04-09 22:34:10 +0530

    Update N_queens_puzzle_solved_codechef.cpp

--- N_queens_puzzle_solved_codechef.cpp
+++ N_queens_puzzle_solved_codechef.cpp
@@ -3,17 +3,17 @@
 #include<iomanip>
 using namespace std;
 
-int main() {
-	// your code goes here
+int main() 
+{
 	int T;
 	cin>>T;
 	while(T--)
 	{
 	    int N;
 	    cin>>N;
-	  float fn=(0.143*N);
-	  float ans=pow(fn,N);
-	    cout<<fixed<<setprecision(0)<<ans<<endl;
+	     float fn=(0.143*N);
+	      float ans=pow(fn,N);
+	      cout<<fixed<<setprecision(0)<<ans<<endl;
 	}
 	return 0;
 }
