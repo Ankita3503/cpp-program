a0ce58ecfcdcd6320e7fbe4a1392ffcc6a9e56d8
ankita <94631478+Ankita3503@users.noreply.github.com>
2022-04-13 21:28:42 +0530

    Create is_the_score_consistent.cpp

---
+++ is_the_score_consistent.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int a,b,c,d;
+	    cin>>a>>b;
+	    cin>>c>>d;
+	    if(a<=c && b<=d)
+	    {
+	       cout<<"POSSIBLE\n"; 
+	    }
+	    else
+	    cout<<"IMPOSSIBLE\n";
+	}
+	return 0;
+}
