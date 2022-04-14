cb1c6f721840cee5e314509765962cf22dfc4d49
ankita <94631478+Ankita3503@users.noreply.github.com>
2022-04-14 23:44:54 +0530

    Create a problem on sticks codechef.cpp

---
+++ a problem on sticks codechef.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    long long t;
+    cin >> t;
+    while (t--)
+    {
+        long long n, count = 0;
+        cin >> n;
+        long long arr[n];
+        for (long long i = 0; i < n; i++)
+        {
+            cin >> arr[i];
+        }
+        sort(arr, arr + n);
+        if (arr[0] != 0)
+            count++;
+        for (long long i = 1; i < n; i++)
+        {
+            if (arr[i] != 0 && arr[i] != arr[i - 1])
+            {
+                count++;
+            }
+        }
+        cout << count << endl;
+    }
+    return 0;
+}
