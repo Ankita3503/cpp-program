357326f4b6cb02952b15857e20cc4ec1dccc8c36
ankita <94631478+Ankita3503@users.noreply.github.com>
2022-04-23 23:23:13 +0530

    Create pakaging_cupakes_codechef.cpp

---
+++ pakaging_cupakes_codechef.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+         int a=(n/2)+1;
+        cout<<a<<endl;
+    }
+	// your code goes here
+	return 0;
+}
