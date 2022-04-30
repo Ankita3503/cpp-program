dbafc31a8df58e4ce063c88043c3443f245ec231
ankita <94631478+Ankita3503@users.noreply.github.com>
2022-04-30 21:59:06 +0530

    Update aaaaqa.cpp

--- aaaaqa.cpp
+++ aaaaqa.cpp
@@ -4,10 +4,10 @@ using namespace std;
 int main()
 {
 	int size;
-	cout<<"enter the size of array 2d \n";
+	cout<<"Enter the size of array 2d \n";
 	cin>>size;
 	int arr[size][size];
-	cout<<"enter the elements of array \n";
+	cout<<"Enter the elements of array \n";
 	for(int i=0;i<size;i++)
 	{
 		for(int j=0;j<size;j++)
